bd73951520e80f2ff14c744135019079675a3f5a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-28 15:43:54 +0300

    @Controller, @ResponseBody

--- src/main/java/by/flameksandr/spring/boot/Springboot1Application.java
+++ src/main/java/by/flameksandr/spring/boot/Springboot1Application.java
@@ -1,13 +1,12 @@
 package by.flameksandr.spring.boot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot1Application.class, args);
+
     }
 
 }
--- src/main/java/by/flameksandr/spring/boot/controllers/HelloController.java
+++ src/main/java/by/flameksandr/spring/boot/controllers/HelloController.java
@@ -1,12 +1,16 @@
 package by.flameksandr.spring.boot.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/hello")//GET, POST, PUT
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    @ResponseBody
     public String hello() {
         return "Hello!!!!";
     }
