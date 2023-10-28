8f14fab3a32428aec0c15f17e79bfcb4bab8f8f4
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-28 15:58:50 +0300

    @GetMapping

--- src/main/java/by/flameksandr/spring/boot/controllers/HelloController.java
+++ src/main/java/by/flameksandr/spring/boot/controllers/HelloController.java
@@ -1,14 +1,11 @@
 package by.flameksandr.spring.boot.controllers;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello/{name}")
+    @GetMapping("/hello/{name}")
     public String hello(@PathVariable("name") String name) {
         return "Hello " + name + "!";
     }
