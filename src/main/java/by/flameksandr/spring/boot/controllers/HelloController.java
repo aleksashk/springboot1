fce4855180b74354991c9578144e3280fce54b5c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-28 15:44:16 +0300

    @RestController

--- src/main/java/by/flameksandr/spring/boot/controllers/HelloController.java
+++ src/main/java/by/flameksandr/spring/boot/controllers/HelloController.java
@@ -1,16 +1,13 @@
 package by.flameksandr.spring.boot.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
-    @ResponseBody
     public String hello() {
         return "Hello!!!!";
     }
